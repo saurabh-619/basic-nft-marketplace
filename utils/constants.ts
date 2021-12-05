24aeb65c99b0fe91c1558d3c5f869ef65656b49c
saurabh-619 <saurabhbomble107@gmail.com>
2021-12-05 18:07:22 +0530

    NFT deployed

--- utils/constants.ts
+++ utils/constants.ts
@@ -2,7 +2,7 @@ export const nftAddress = process.env.NEXT_PUBLIC_NFT_ADDRESS as string;
 export const nftMarketAddress = process.env
   .NEXT_PUBLIC_NFT_MARKET_ADDRESS as string;
 
-import NFT from "../artifacts/contracts/NFT.sol/NFT.json";
-import NFTMarket from "../artifacts/contracts/NFTMarket.sol/NFTMarket.json";
+import NFT from "./NFT.json";
+import NFTMarket from "./NFTMarket.json";
 
 export { NFT, NFTMarket };
